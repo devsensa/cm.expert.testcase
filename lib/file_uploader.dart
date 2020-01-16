681e6cea2162ce2c830ba2a5a13d5d70b3505e40
devsensa <person.sensa@gmail.com>
2020-01-17 01:11:41 +0500

    Change FileUploader api & implementation

--- lib/file_uploader.dart
+++ lib/file_uploader.dart
@@ -1,19 +1,20 @@
 import "dart:math";
 import "dart:typed_data";
 import "./file.dart";
+import "./uploading_process.dart";
 
 class FileUploader {
-  Future<File> fetchFile(String fileName) {
+  Future<UploadingProcess> fetchFile(UploadingProcess process) {
     var rnd = Random();
     var delay = rnd.nextInt(4) + 1;
     return Future.delayed(Duration(seconds: delay), () {
-      return File(fileName, Uint8List(rnd.nextInt(255)));
+      var file = File(process.fileName, Uint8List(rnd.nextInt(255)));
+      process.completeWithFile(file);
+      return process;
     });
   }
 
-  Future<List<File>> fetch(List<String> names) {
-    return Future.wait(names.map((String name) {
-      return fetchFile(name);
-    }));
+  Future<List<UploadingProcess>> fetch(List<UploadingProcess> processes) {
+    return Future.wait(processes.map((UploadingProcess process) => fetchFile(process)));
   }
 }
