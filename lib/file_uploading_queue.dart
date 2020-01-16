750784f3d876877863f0097342fa6cfd1688e22e
devsensa <person.sensa@gmail.com>
2020-01-17 01:13:50 +0500

    WIP Implement FileUploadingQueue

--- lib/file_uploading_queue.dart
+++ lib/file_uploading_queue.dart
@@ -1,12 +1,31 @@
 import "dart:async";
+import "dart:collection";
+import "./uploading_process.dart";
+import "./file_lifecycle.dart";
 
 class FileUploadingQueue {
   int _fileAddLimit;
   int _concurrentUploadingLimit;
+  ListQueue<UploadingProcess> _queue = ListQueue(15);
 
-  FileUploadingQueue({int fileAddLimit = 30, int concurrentUploadingLimit = 3})
-      : _fileAddLimit = fileAddLimit,
-        _concurrentUploadingLimit = concurrentUploadingLimit;
+  StreamController<bool> _emptyStream = StreamController.broadcast();
+  StreamController<bool> _uploadingStream = StreamController.broadcast();
+  StreamController<UploadingState> _uploadingStateStream =
+      StreamController.broadcast();
+  StreamController<bool> _canAddFileStream = StreamController.broadcast();
+  StreamController<List<UploadingProcess>> _processesStream =
+      StreamController.broadcast();
+
+  FileUploadingQueue(
+      {int fileAddLimit = 30, int concurrentUploadingLimit = 3}) {
+    _fileAddLimit = fileAddLimit;
+    _concurrentUploadingLimit = concurrentUploadingLimit;
+    _emptyStream.add(false);
+    _uploadingStream.add(false);
+    _uploadingStateStream.add(UploadingState(0, 0));
+    _canAddFileStream.add(true);
+    _processesStream.add([]);
+  }
 
 /* --------------------------------- Getters -------------------------------- */
   int get fileAddLimit {
@@ -25,15 +44,55 @@ class FileUploadingQueue {
 
 /* ------------------------------- Operations ------------------------------- */
 
-  Future<void> cancel() async {}
-  Future<void> save() async {}
-  Future<void> add(String fileName) async {}
-  Future<void> delete(String filename) async {}
+  Future<void> cancel() async {
+    _queue.clear();
+    updateEmptyStream();
+  }
+
+  Future<void> save() async {
+    return;
+  }
+
+  Future<void> add(String fileName) async {
+    var process = UploadingProcess(fileName);
+    _queue.add(process);
+  }
+
+  Future<void> delete(int hashCode) async {
+    _queue.removeWhere(
+        (UploadingProcess process) => process.hashCode == hashCode);
+  }
+
+/* ------------------------------ Util methods ------------------------------ */
+  void updateEmptyStream() {
+    _emptyStream.add(_queue.isEmpty);
+  }
+
+  void updateUploadingStream() {
+    _uploadingStream.add(_queue.any((UploadingProcess process) =>
+        process.status != FileLifecycle.uploaded));
+  }
+
+  void updateUploadingStateStream() {
+    var allFiles = _queue.length;
+    var inProcess = _queue
+        .where((UploadingProcess process) =>
+            process.status != FileLifecycle.uploaded)
+        .length;
+    _uploadingStateStream.add(UploadingState(inProcess, allFiles));
+  }
+
+  void updateCanAddFileStream() {
+    _canAddFileStream.add(_queue.length < _fileAddLimit);
+  }
+
+  void updateProcessesStream() {
+    _processesStream.add(_queue.toList(growable: false));
+  }
 }
 
 class UploadingState {
   int filesInProcessCount;
   int allFilesCount;
+  UploadingState(this.filesInProcessCount, this.allFilesCount);
 }
-
-class UploadingProcess {}
