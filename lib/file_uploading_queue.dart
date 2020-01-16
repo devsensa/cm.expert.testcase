39a1bd43dbb5fd4028e1e00983cdad493dd361b5
devsensa <person.sensa@gmail.com>
2020-01-16 22:22:42 +0500

    Update FileUploadingQueue API

--- lib/file_uploading_queue.dart
+++ lib/file_uploading_queue.dart
@@ -3,13 +3,19 @@ import "dart:async";
 class FileUploadingQueue {
   Stream<bool> empty() {}
   Stream<bool> uploading() {}
-  Stream<UploadingState> uploadingState;
+  Stream<UploadingState> uploadingState() {}
+  Stream<bool> canAddFile() {}
+  Stream<List<UploadingProcess>> processes() {}
 
   Future<void> cancel() async {}
   Future<void> save() async {}
+  Future<void> add(String fileName) async {}
+  Future<void> delete(String filename) async {}
 }
 
 class UploadingState {
   int filesInProcessCount;
   int allFilesCount;
 }
+
+class UploadingProcess {}
