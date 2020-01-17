41f2b131faebec6077fae3d01bf2131e3ff95283
devsensa <person.sensa@gmail.com>
2020-01-17 13:59:29 +0500

    Implement AddButton

---
+++ lib/add_button.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+import "package:property_change_notifier/property_change_notifier.dart";
+import "package:faker/faker.dart";
+
+import "./file_uploading_queue.dart";
+
+class AddButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return PropertyChangeConsumer<FileUploadingQueue>(
+        builder: (context, model, properties) {
+      var addAction = () => model.add("${faker.lorem.word()}.txt");
+      return FloatingActionButton(
+        onPressed: model.canAddFile ? addAction : null,
+        tooltip: 'Add file',
+        child: Icon(Icons.add),
+      );
+    });
+  }
+}
