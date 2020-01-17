46c4085b98b42344fe472c5239149e6eec679e1f
devsensa <person.sensa@gmail.com>
2020-01-17 14:58:17 +0500

    Implement notification for SaveButton

--- lib/save_button.dart
+++ lib/save_button.dart
@@ -6,11 +6,24 @@ import './file_uploading_queue.dart';
 class SaveButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return PropertyChangeConsumer<FileUploadingQueue>(
-        builder: (context, model, properties) {
-      return FlatButton(
-        child: Text('Сохранить'),
-        onPressed: model.empty || model.uploading ? null : model.save,
-      );
-    });
+      builder: (context, model, properties) {
+        var handlePress = () async {
+          model.save().then((value) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text("Файлы сохранены"),
+                );
+              },
+            );
+          });
+        };
+        return FlatButton(
+          child: Text('Сохранить'),
+          onPressed: model.empty || model.uploading ? null : handlePress,
+        );
+      },
+    );
   }
 }
