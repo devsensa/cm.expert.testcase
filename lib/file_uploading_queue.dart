a1190998066a1f8996624430b6915798c437d46c
devsensa <person.sensa@gmail.com>
2020-01-17 09:37:38 +0500

    Change FileUploadingQueue api & implementation for PropertyChangeNotifier

--- lib/file_uploading_queue.dart
+++ lib/file_uploading_queue.dart
@@ -1,31 +1,18 @@
 import "dart:async";
 import "dart:collection";
+
+import "package:property_change_notifier/property_change_notifier.dart";
 import "./uploading_process.dart";
 import "./file_lifecycle.dart";
 
-class FileUploadingQueue {
+class FileUploadingQueue extends PropertyChangeNotifier<String> {
   int _fileAddLimit;
   int _concurrentUploadingLimit;
   ListQueue<UploadingProcess> _queue = ListQueue(15);
 
-  StreamController<bool> _emptyStream = StreamController.broadcast();
-  StreamController<bool> _uploadingStream = StreamController.broadcast();
-  StreamController<UploadingState> _uploadingStateStream =
-      StreamController.broadcast();
-  StreamController<bool> _canAddFileStream = StreamController.broadcast();
-  StreamController<List<UploadingProcess>> _processesStream =
-      StreamController.broadcast();
-
-  FileUploadingQueue(
-      {int fileAddLimit = 30, int concurrentUploadingLimit = 3}) {
-    _fileAddLimit = fileAddLimit;
-    _concurrentUploadingLimit = concurrentUploadingLimit;
-    _emptyStream.add(false);
-    _uploadingStream.add(false);
-    _uploadingStateStream.add(UploadingState(0, 0));
-    _canAddFileStream.add(true);
-    _processesStream.add([]);
-  }
+  FileUploadingQueue({int fileAddLimit = 30, int concurrentUploadingLimit = 3})
+      : _fileAddLimit = fileAddLimit,
+        _concurrentUploadingLimit = concurrentUploadingLimit;
 
 /* --------------------------------- Getters -------------------------------- */
   int get fileAddLimit {
@@ -36,59 +23,47 @@ class FileUploadingQueue {
     return _concurrentUploadingLimit;
   }
 
-  Stream<bool> empty() {}
-  Stream<bool> uploading() {}
-  Stream<UploadingState> uploadingState() {}
-  Stream<bool> canAddFile() {}
-  Stream<List<UploadingProcess>> processes() {}
-
-/* ------------------------------- Operations ------------------------------- */
-
-  Future<void> cancel() async {
-    _queue.clear();
-    updateEmptyStream();
-  }
-
-  Future<void> save() async {
-    return;
-  }
-
-  Future<void> add(String fileName) async {
-    var process = UploadingProcess(fileName);
-    _queue.add(process);
-  }
-
-  Future<void> delete(int hashCode) async {
-    _queue.removeWhere(
-        (UploadingProcess process) => process.hashCode == hashCode);
-  }
-
-/* ------------------------------ Util methods ------------------------------ */
-  void updateEmptyStream() {
-    _emptyStream.add(_queue.isEmpty);
-  }
-
-  void updateUploadingStream() {
-    _uploadingStream.add(_queue.any((UploadingProcess process) =>
-        process.status != FileLifecycle.uploaded));
-  }
-
-  void updateUploadingStateStream() {
+  bool get empty => _queue.isEmpty;
+  bool get uploading => _queue.any(
+      (UploadingProcess process) => process.status != FileLifecycle.uploaded);
+  UploadingState get uploadingState {
     var allFiles = _queue.length;
     var inProcess = _queue
         .where((UploadingProcess process) =>
             process.status != FileLifecycle.uploaded)
         .length;
-    _uploadingStateStream.add(UploadingState(inProcess, allFiles));
+    return UploadingState(inProcess, allFiles);
   }
 
-  void updateCanAddFileStream() {
-    _canAddFileStream.add(_queue.length < _fileAddLimit);
+  bool get canAddFile => _queue.length < _fileAddLimit;
+  List<UploadingProcess> get processes => _queue.toList(growable: false);
+
+/* ------------------------------- Operations ------------------------------- */
+
+  Future<void> cancel() async {
+    _queue.clear();
+    notifyListeners('empty');
   }
 
-  void updateProcessesStream() {
-    _processesStream.add(_queue.toList(growable: false));
+  Future<void> save() async {
+    return;
+    notifyListeners();
   }
+
+  Future<void> add(String fileName) async {
+    var process = UploadingProcess(fileName);
+    _queue.add(process);
+    notifyListeners();
+  }
+
+  Future<void> delete(int hashCode) async {
+    _queue.removeWhere(
+        (UploadingProcess process) => process.hashCode == hashCode);
+    notifyListeners();
+  }
+
+/* ------------------------------ Util methods ------------------------------ */
+
 }
 
 class UploadingState {
