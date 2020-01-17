82d1abfa7459497a847b2b53a4095fa55d5e4dbd
devsensa <person.sensa@gmail.com>
2020-01-17 12:51:43 +0500

    Implement FilesSubtitle

---
+++ lib/files_subtitle.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import "package:property_change_notifier/property_change_notifier.dart";
+import "./file_uploading_queue.dart";
+
+class FilesSubtitle extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return PropertyChangeConsumer(builder: (context, model, properties) {
+      var displayText = "";
+      if (model.empty) displayText = "Нет файлов";
+      model.lengthOfProcessing == 0
+          ? "Кол-во файлов: ${model.length}"
+          : "Осталось загрузить: ${model.lengthOfProcessing}. Всего файлов: ${model.length}";
+      return Text(displayText);
+    });
+  }
+}
